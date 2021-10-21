3304933fdd027556ee2f788609e3e5a5f41e022a
Hotori Amano <amanohotori@gmail.com>
2021-10-21 13:26:58 +0900

    Class data型 ”Vegetable” が get した文字列を .ToString で返す

--- Program.cs
+++ Program.cs
@@ -3,12 +3,22 @@ using System;
 
 namespace interpolated
 {
-    class Program
+    public class Vegetable
     {
+        public Vegetable(string name) => Name = name;
+        public string Name { get; }
+        public override string ToString() => Name;
+    }
+    public class Program
+    {
+        public enum Unit { item, kilogram, gram, dozen };
         static void Main(string[] args)
         {
-            Object obj = new Object();
-            Console.WriteLine(obj.ToString());
+            var item = new Vegetable("eggplant");
+            var date = DateTime.Now;
+            var price = 1.99m;
+            var unit = Unit.item;
+            Console.WriteLine($"On {date}, the price of {item} was {price} per {unit}.");
         }
     }
 }
