62963865fe8bcbb6a5fa6dff0b88431ec7429aeb
Hotori Amano <amanohotori@gmail.com>
2021-10-21 12:00:30 +0900

    Accept Incoming Changeを選んでブランチとのマージで競合をmaster側に修正

--- Program.cs
+++ Program.cs
@@ -7,13 +7,8 @@ namespace interpolated
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            var name = "manaru更に変更";
-            Console.WriteLine($"Hello, {name}. It's a pleasure to meet you!");
-=======
             Object obj = new Object();
             Console.WriteLine(obj.ToString());
->>>>>>> 8002b16545384b7b4e79614de9b70f48bcc158a6
         }
     }
 }
