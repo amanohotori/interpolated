751b3645ab8224f7e8a332cfabf145e3ce2174eb
Hotori Amano <amanohotori@gmail.com>
2021-10-21 11:11:38 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace interpolated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "hotori";
+            Console.WriteLine($"Hello, {name}. It's a pleasure to meet you!");
+        }
+    }
+}
